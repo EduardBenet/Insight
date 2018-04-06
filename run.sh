bc03c96a5a636a5f714eaafe922e9da9e07bba16
EduardBenet <38017692+EduardBenet@users.noreply.github.com>
2018-04-06 04:53:51 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
+python ./src/sessionization3.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
